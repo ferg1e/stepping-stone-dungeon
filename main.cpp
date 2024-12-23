3604950e1e3bacc474b42e3175bc149ccb1deb6a
Ry Ferguson <rytf12@gmail.com>
2024-12-23 22:36:17 +0000

    enable 'back' command from about screen; clear whole screen in all places

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ void home();
 void newGame();
 void about();
 
+void clearScreen() {
+    system("clear");
+}
+
 void clearLines(int count) {
     while(count-- > 0) {
         std::cout << "\x1b[1F" << "\x1b[2K";
@@ -22,7 +26,7 @@ void home() {
 
     getline(std::cin, action);
 
-    clearLines(4);
+    clearScreen();
 
     if(action == "N" || action == "n") {
         newGame();
@@ -49,7 +53,7 @@ void newGame() {
 
     getline(std::cin, action);
 
-    clearLines(4);
+    clearScreen();
 
     if(action == "K" || action == "k") {
         std::cout << "Knight!" << std::endl;
@@ -66,17 +70,19 @@ void newGame() {
 void about() {
     std::cout << "Binary Dungeon Tree is a text-based dungeon crawler. The dungeon is shaped like a binary tree with each node being a room. The root room (room 1) is where you select your character. From there you can take the left door to go down to room 2, or the right door to go down to room 3. Room 2 leads to room 4 or 5, and room 3 leads to room 6 or 7. So on and so forth. Each room besides room 1 is either a monster, a shop or a warp zone." << std::endl << std::endl << "[B]ack" << std::endl;
 
-    /*std::string action;
+    std::string action;
 
     while(action != "B" && action != "b") {
         getline(std::cin, action);
+        clearLines(1);
     }
 
-    clearLines(4);*/
-
+    clearScreen();
+    home();
 }
 
 int main() {
+    clearScreen();
     home();
 
     std::cout << "End of Game..." << std::endl;
